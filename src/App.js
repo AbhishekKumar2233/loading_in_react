8a8d9413f7f86e8fd8ac92e066e6200a3d709cd9
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-29 19:23:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+import ReactLoading from "react-loading";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ReactLoading type="balls" color="#0000FF" height={100} width={50} />
+      <ReactLoading type="bars" color="#0000FF" height={100} width={50} />
+      <ReactLoading type="bubbles" color="#0000FF" height={100} width={50} />
+      <ReactLoading type="cubes" color="#0000FF" height={100} width={50} />
+      <ReactLoading type="cylon" color="#0000FF" height={100} width={50} />
+      <ReactLoading type="spin" color="#0000FF" height={100} width={50} />
+      <ReactLoading type="spokes" color="#0000FF" height={100} width={50} />
+      <ReactLoading
+        type="spinningBubbles"
+        color="#0000FF"
+        height={100}
+        width={50}
+      />
+    </div>
+  );
+}
